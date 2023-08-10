03414894ee78190614db276e9e5eccf9cd6f575e
Lerish-vZ <lerishavz@gmail.com>
2023-08-10 11:12:50 +0200

    Update

--- Chapter_5/src/App.js
+++ Chapter_5/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import Products from "./Products";
+//import Products from "./Products";
 import JumboTronComponent from './JumboTronComponent';
 class App extends Component {
   render() {
     return (
       <div>
         <h1>My First React App!</h1>
-        <Products />
+        {/* <Products /> */}
+        <JumboTronComponent />
       </div>
     );
   }
