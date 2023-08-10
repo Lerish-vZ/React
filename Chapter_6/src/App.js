65b4122caab611881bdc2da67ad061d6f01e740b
Lerish-vZ <lerishavz@gmail.com>
2023-08-10 11:50:20 +0200

    Update

--- Chapter_6/src/App.js
+++ Chapter_6/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import JumboTronComponent from './JumboTronComponent';
-// import UserForm from './UserForm';
+import UserForm from './UserForm';
 //import Products from './Products';
 
 class App extends Component {
