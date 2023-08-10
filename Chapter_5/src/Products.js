05fd5c12e23f0216a73ce0860302c10a93aaf5b2
Lerish-vZ <lerishavz@gmail.com>
2023-08-10 11:06:35 +0200

    Update

--- Chapter_5/src/Products.js
+++ Chapter_5/src/Products.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 class Products extends Component {
   render() {
     const products = ["Learning React", "Pro React", "Beginning React"];
+    //const products = [];
     const listProducts = products.map((product) => (
       <li key={product.toString()}>{product}</li>
     ));
     return (
       <div>
-        {listProducts.length > 0 && <ul>{listProducts}</ul>}
-        {listProducts.length == 0 && <ul>No Products to display</ul>}
+        {/* {listProducts.length > 0 && <ul>{listProducts}</ul>}
+        {listProducts.length == 0 && <ul>No Products to display</ul>} */}
+
+        {listProducts.length > 0 ? (
+          <ul>{listProducts}</ul>
+        ) : (
+          <ul>No Products to display</ul>
+        )}
       </div>
     );
   }
