00ae21cb9880270286268acb3ce5813e97dccccd
Lerish-vZ <lerishavz@gmail.com>
2023-08-10 12:03:21 +0200

    Update

--- Chapter_6/src/UserForm.js
+++ Chapter_6/src/UserForm.js
@@ -18,6 +18,13 @@ class UserForm extends Component {
               !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
             ) {
               errors.email = "Invalid email address";
+            } else if (values.email.length < 10) {
+              errors.email = "Email address too short";
+            }
+            if (!values.password) {
+              errors.password = "Required";
+            } else if (values.password.length < 8) {
+              errors.password = "Password too short";
             }
             return errors;
           }}
