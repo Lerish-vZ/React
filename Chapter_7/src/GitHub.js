e9c6038a5cc999e1ac6148e9c4994cb3b701d3f6
Lerish-vZ <lerishavz@gmail.com>
2023-08-11 12:56:05 +0200

    Update

--- Chapter_7/src/GitHub.js
+++ Chapter_7/src/GitHub.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios"; // npm install axios
+import ReactLoading from "react-loading";
+import { Media, Form, FormGroup, FormControl, Button } from 'reactbootstrap';
 class GitHub extends Component {
   constructor() {
     super();
     this.state = {
+      data: [],
       isLoading: true,
     };
     this.getGitHubData("greg"); //returns promise which we need to subscribe to
@@ -15,12 +18,41 @@ class GitHub extends Component {
         //subscribe to promise (when ajax call is completed the response is fed to the promise then pushed to the component)
         this.setState({
           isLoading: false, //not loading anymore got results form server
+          data: res.data.items,
         });
         console.log(res.data.items);
       });
   }
   render() {
-    return <div>{this.state.isLoading && <h4>Getting data...</h4>}</div>; //add load icon
+    const listUsers = this.state.data.map((user) => (
+      <Media key={user.id}>
+        <a href={user.html_url}>
+          <img
+            width={64}
+            height={64}
+            className="mr-3"
+            src={user.avatar_url}
+            alt="Generic placeholder"
+          />
+        </a>
+        <Media.Body>
+          <h5>Login: {user.login}</h5>
+          <p>Id: {user.id}</p>
+        </Media.Body>
+      </Media>
+    ));
+    return (
+      <div>
+        <h3>GitHub Users Results</h3>
+        {
+          this.state.isLoading && (
+            <ReactLoading type="spinningBubbles" color="#444" />
+          )
+          // <h4>Getting data...</h4>
+        }
+        {listUsers}
+      </div>
+    ); //add load icon
   }
 }
 export default GitHub;
