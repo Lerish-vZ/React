d0a11074f2d23e4f88c0d9ef615217a609d8044c
Lerish-vZ <lerishavz@gmail.com>
2023-08-11 10:51:59 +0200

    Update

--- Chapter_7/src/GitHub.js
+++ Chapter_7/src/GitHub.js
@@ -3,12 +3,12 @@ import axios from "axios"; // npm install axios
 class GitHub extends Component {
   constructor() {
     super();
-    this.getGitHubData("greg");
+    this.getGitHubData("greg"); //returns promise which we need to subscribe to 
   }
   getGitHubData(_searchTerm) {
     axios
-      .get("https://api.github.com/search/users?q=" + _searchTerm)
-      .then((res) => {
+      .get("https://api.github.com/search/users?q=" + _searchTerm) //use get of axios and give the url of API endpoint
+      .then((res) => { //subscribe to promise (when ajax call is completed the response is fed to the promise then pushed to the component)
         console.log(res.data.items);
       });
   }
