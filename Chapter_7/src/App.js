ed7eb76a95278ff6bdda42bc6bbba853aa20c447
Lerish-vZ <lerishavz@gmail.com>
2023-08-11 12:49:32 +0200

    Update

--- Chapter_7/src/App.js
+++ Chapter_7/src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import React, { Component } from 'react';
-import JumboTronComponent from './JumboTronComponent';
-import UserForm from './UserForm';
+import "./App.css";
+import React, { Component } from "react";
+import JumboTronComponent from "./JumboTronComponent";
+import UserForm from "./UserForm";
 //import Products from './Products';
+import GitHub from "./GitHub";
 
 class App extends Component {
-  formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-  }
+  //   formatName(user) {
+  //     return user.firstName + ' ' + user.lastName;
+  //   }
+
+  //   render() {
+
+  //     return (
+  //       <div className="App">
+  //         <JumboTronComponent>
+  //           This is a long sentence, and I want to insert content into the
+  //           jumbotron component from the outside.
+  //         </JumboTronComponent>
+  //         <UserForm></UserForm>
+  //         {/* <Products /> */}
+  //       </div>
+  //     );
+  //   }
+  // }
 
   render() {
-
     return (
-      <div className="App">
-        <JumboTronComponent>
-          This is a long sentence, and I want to insert content into the
-          jumbotron component from the outside.
-        </JumboTronComponent>
-        <UserForm></UserForm>
-        {/* <Products /> */}
+      <div>
+        <GitHub />
       </div>
     );
   }
 }
 
-export default App
+export default App;
--- Chapter_7/src/GitHub.js
+++ Chapter_7/src/GitHub.js
@@ -3,17 +3,24 @@ import axios from "axios"; // npm install axios
 class GitHub extends Component {
   constructor() {
     super();
-    this.getGitHubData("greg"); //returns promise which we need to subscribe to 
+    this.state = {
+      isLoading: true,
+    };
+    this.getGitHubData("greg"); //returns promise which we need to subscribe to
   }
   getGitHubData(_searchTerm) {
     axios
       .get("https://api.github.com/search/users?q=" + _searchTerm) //use get of axios and give the url of API endpoint
-      .then((res) => { //subscribe to promise (when ajax call is completed the response is fed to the promise then pushed to the component)
+      .then((res) => {
+        //subscribe to promise (when ajax call is completed the response is fed to the promise then pushed to the component)
+        this.setState({
+          isLoading: false, //not loading anymore got results form server
+        });
         console.log(res.data.items);
       });
   }
   render() {
-    return <div></div>;
+    return <div>{this.state.isLoading && <h4>Getting data...</h4>}</div>; //add load icon
   }
 }
 export default GitHub;
