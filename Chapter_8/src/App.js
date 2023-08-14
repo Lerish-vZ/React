d35d2232b06d7a339a211295998ccf111ea8521b
Lerish-vZ <lerishavz@gmail.com>
2023-08-14 13:21:52 +0200

    Update

--- Chapter_8/src/App.js
+++ Chapter_8/src/App.js
@@ -1,64 +1,60 @@
-import "./App.css";
-import JumboTronComponent from "./JumboTronComponent";
-import UserForm from "./UserForm";
-import Products from './Products';
-import React, { Component } from "react";
-import GitHub from "./GitHub";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { Component } from 'react';
+import GitHub from './GitHub';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 import GitHubUser from './GitHubUser';
 
-class App extends Component {
-  render() {
+
+class App extends Component {       
+  render() {        
     return (
       <div>
         <Header />
-        {/* render a header component */}
       </div>
     );
   }
 }
-
 export default App;
 
 class Header extends Component {
-  render() {
+  render(){
     return (
-      <BrowserRouter>
-        {/* contains Routes that tell router where to go once window's location changes */}
-        <div>
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/github">GitHub</Nav.Link>
-                <Nav.Link href="/Test">Test</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Switch>
-            {/* Wrap routes in Switch as additional measure to not show more than one component simultaneously */}
-            <Route path="/github/user/:login/:id" component={GitHubUser} />
-            <Route path="/github" component={GitHub} />
-            <Route exact path="/" component={Home} />
-            <Route path="/*" component={NotFound} />
-          </Switch>
-          {/* Each route has at least two properties: path, and a unique name we assign to our route + a component that specifies the associated component */}
-        </div>
-      </BrowserRouter>
-    );
-  } //BrowserRoute, Switch and Route is imported from 'react-router-dom'
+        <BrowserRouter>
+          <div>            
+            <Navbar bg="light" expand="lg">
+              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/github">GitHub</Nav.Link>                  
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Switch> 
+              <Route path="/github/user/:login/:id" component={GitHubUser} />                                                                
+              <Route path="/github" component={GitHub} />              
+              <Route exact path="/" component={Home} />             
+              <Route path="/*" component={NotFound} />                       
+            </Switch>   
+          </div>  
+        </BrowserRouter>              
+    )
+  }
 }
+
 class Home extends Component {
-  render() {
-    return <div>Home</div>;
+  render(){
+    return (
+      <div>
+        Home        
+      </div>
+    )
   }
 }
 
 class NotFound extends Component {
-  render() {
-    return <div>Not Found</div>;
+  render(){
+    return <div>Not Found</div>
   }
 }
