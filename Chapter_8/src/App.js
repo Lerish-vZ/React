9caa5348c8383f69dfc72a53bb3a9a85d4196e96
Lerish-vZ <lerishavz@gmail.com>
2023-08-14 11:05:49 +0200

    Update

--- Chapter_8/src/App.js
+++ Chapter_8/src/App.js
@@ -6,12 +6,13 @@ import React, { Component } from "react";
 import GitHub from "./GitHub";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Nav, Navbar } from 'react-bootstrap';
+import GitHubUser from './GitHubUser';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header /> 
+        <Header />
         {/* render a header component */}
       </div>
     );
@@ -23,26 +24,50 @@ export default App;
 class Header extends Component {
   render() {
     return (
-      // <BrowserRouter> 
-      // {/* contains Routes that tell router where to go once window's location changes */}
-      //   <div>
-      //     <Switch>
-      //       {/* Wrap routes in Switch as additional measure to not show more than one component simultaneously */}
-      //       <Route path="/github" component={GitHub} />
-      //       <Route exact path="/" component={Home} />
-      //       <Route path="/*" component={NotFound} />
-      //     </Switch>
-      //     {/* Each route has at least two properties: path, and a unique name we assign to our route + a component that specifies the associated component */}
-      //   </div>
-      // </BrowserRouter>
-
-
+      <BrowserRouter>
+        {/* contains Routes that tell router where to go once window's location changes */}
+        <div>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/github">GitHub</Nav.Link>
+                <Nav.Link href="/Test">Test</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Switch>
+            {/* Wrap routes in Switch as additional measure to not show more than one component simultaneously */}
+            <Route path="/github/user/:login/:id" component={GitHubUser} />
+            <Route path="/github" component={GitHub} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Test" component={Test} />
+            <Route path="/*" component={NotFound} />
+          </Switch>
+          {/* Each route has at least two properties: path, and a unique name we assign to our route + a component that specifies the associated component */}
+        </div>
+      </BrowserRouter>
     );
   } //BrowserRoute, Switch and Route is imported from 'react-router-dom'
 }
 class Home extends Component {
   render() {
     return <div>Home</div>;
+  }
+}
+class Test extends Component{
+  render() {
+    return(
+      <div>
+        <h1> This is my own path that I created called...</h1>
+        <h2> Test </h2>
+        <h3> Test </h3>
+        <h4> Test </h4>
+        <p> Test </p>
+      </div>
+    )
   }
 }
 class NotFound extends Component {
