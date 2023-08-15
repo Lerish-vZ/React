f48f91b048e6dcf8948d95df2913c2ac8186a8d3
Lerish-vZ <lerishavz@gmail.com>
2023-08-15 11:24:11 +0200

    Update

--- Chapter_9/src/index.js
+++ Chapter_9/src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as firebase from "firebase";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+var config = {
+  apiKey: "AIzaSyAyzOM04_YdHDnD2qHkS8bYcVIz2-VMpGA",
+  authDomain: "chapter9-5ee23.firebaseapp.com",
+  projectId: "chapter9-5ee23",
+  storageBucket: "chapter9-5ee23.appspot.com",
+  messagingSenderId: "1037124581890",
+  appId: "1:1037124581890:web:5489e1540d8c5a0a23c51b",
+  measurementId: "G-DK7EZQN74P",
+};
+
+firebase.initializeApp(config);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
