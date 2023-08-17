fb82ab18a92c006281e3c96d68c15c0235c50f70
Lerish-vZ <lerishavz@gmail.com>
2023-08-17 14:50:38 +0200

    Update

--- Chapter_9/src/App.js
+++ Chapter_9/src/App.js
@@ -20,10 +20,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 class App extends Component {
   constructor() {
     super();
-    console.log(firebaseConfig);
+    console.log(app);
   }
   render() {
     return (
