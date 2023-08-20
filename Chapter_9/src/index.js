98ea64618dc39f451f48d1d6fc9a01ae1dd38f1f
Lerish-vZ <lerishavz@gmail.com>
2023-08-20 12:29:32 +0200

    Update

--- Chapter_9/src/index.js
+++ Chapter_9/src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+
+
+var config = {
   apiKey: "AIzaSyAyzOM04_YdHDnD2qHkS8bYcVIz2-VMpGA",
   authDomain: "chapter9-5ee23.firebaseapp.com",
   projectId: "chapter9-5ee23",
@@ -20,10 +18,7 @@ const firebaseConfig = {
   appId: "1:1037124581890:web:5489e1540d8c5a0a23c51b",
   measurementId: "G-DK7EZQN74P"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(config);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
