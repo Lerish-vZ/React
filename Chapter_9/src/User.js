f3baf7cf3352f2d3b9aa8e61dadada0eb2f26077
Lerish-vZ <lerishavz@gmail.com>
2023-08-20 13:03:52 +0200

    Update

---
+++ Chapter_9/src/User.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    firebase
+      .database()
+      .ref("/")
+      .on("value", (snapshot) => {
+        console.log(snapshot.val());
+      });
+  }
+  render() {
+    return <div></div>;
+  }
+}
+export default User;
