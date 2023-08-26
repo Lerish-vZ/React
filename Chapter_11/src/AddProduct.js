4eb0c3c89fd1a2bf3b53a02a9b2d807d494525ee
Lerish-vZ <lerishavz@gmail.com>
2023-08-26 13:52:37 +0200

    Update

--- Chapter_11/src/AddProduct.js
+++ Chapter_11/src/AddProduct.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+
 class AddProduct extends Component {
+
   state = {
     productName: "",
     productPrice: 0,
   };
+
   productNameChangedHandler = (event) => {
     this.setState({ productName: event.target.value });
   };
+  
   productPriceChangedHandler = (event) => {
     this.setState({ productPrice: event.target.value });
   };
+
   render() {
     return (
       <div className="container">
